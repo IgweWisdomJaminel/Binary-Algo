5b079b6ab4b27d72969b1782faac45efe660a258
Igwe Wisdom <150636904+IgweWisdomJaminel@users.noreply.github.com>
2024-04-22 22:04:02 +0100

    Update Day10.java

--- src/Day10.java
+++ src/Day10.java
@@ -1,33 +1,66 @@
 import java.util.*;
 
 public class Day10 {
+
+
+    public static void hackerRankSolution(){
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int count=0;
+        int max=0;
+        while(n>0){
+            if(n%2==1){
+                count++;
+
+                if(count>max){
+                    max=count;
+                }
+                else{
+                    count=0;
+                }
+
+            }
+            n=  n/2;
+        }
+
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int sum=0;
+        int sum = 0;
 
         List<Integer> list = new ArrayList<>();
-        while(n>0) {
+        while (n > 0) {
             int remainder = n % 2;
             n = n / 2;
 
-           list.add(remainder);
+            list.add(remainder);
+
 
         }
         Collections.reverse(list);
         System.out.println(list.toString());
-        for(int basetwo:list){
 
-            sum +=basetwo;
+
+        int count = 0;
+        int max=0;
+
+        for(int basetwo : list) {
             if (basetwo == 0) {
-               break;
+                count = 0;
+
+            }else {
+                count++;
+                if(count>max){
+                    max = count;
+                }
+
             }
 
 
-
-
         }
-        System.out.println(sum);
-        }
+        System.out.println(max);
+    }
+
     }
 
